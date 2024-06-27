f17aeeed6559c33ac97cd66d3db78c778132ff69
Kevin M <k.mary@live.be>
2024-06-27 09:33:59 +0200

    MAJ du service pour utilisation de signal

--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import type { InvestmentInput } from './investment-input.model';
 
 @Injectable({ providedIn: 'root' })
 export class InvestmentService {
-    resultData?: {
+    resultData = signal<{
         year: number;
         interest: number;
         valueEndOfYear: number;
         annualInvestment: number;
         totalInterest: number;
         totalAmountInvested: number;
-    }[];
+    }[] | undefined>(undefined);
 
     calculateInvestmentResults(data: InvestmentInput) {
         const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
@@ -33,6 +33,6 @@ export class InvestmentService {
                 totalAmountInvested: initialInvestment + annualInvestment * year,
             });
         }
-        this.resultData = annualData;
+        this.resultData.set(annualData);
     }
 } 
